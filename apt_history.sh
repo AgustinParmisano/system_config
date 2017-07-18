a0f41af92438133028d068a2cd88992f700b968c
root <root@debian>
2017-07-18 14:25:29 -0300

    Adding apt_history.sh wich has apt installations.

---
+++ apt_history.sh
@@ -0,0 +1,27 @@
+!#/bin/bash
+apt update
+apt install -y build-essential
+apt install -f
+apt autoremove
+apt  install zsh
+apt install curl
+apt install git
+apt install tmux
+apt install -y nodejs\n
+apt install -y nodejs
+apt install -y build-essential
+apt install virtualenv
+apt install -f
+apt autoremove
+apt update
+apt install build-essential
+apt update
+apt install -y nodejs
+apt install -y nodejs
+apt install -f
+apt autoremove
+apt update
+apt install gparted
+apt install unetbootin
+apt install postgresql
+apt install locales
